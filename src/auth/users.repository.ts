0b5e543fba2a7beefe5b507eaf0f83a935a0a3e5
nigmasilmi <sn.silmi@gmail.com>
2021-10-21 17:47:44 -0300

    Auth implemented with guarded routes

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -9,6 +9,7 @@ import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
+  // create user
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     // hashing
@@ -16,6 +17,7 @@ export class UsersRepository extends Repository<User> {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const user = this.create({ username, password: hashedPassword });
+
     try {
       await this.save(user);
     } catch (error) {
