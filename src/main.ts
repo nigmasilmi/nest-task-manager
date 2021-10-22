961ea220bb4311c289200c0419b3c791cd52518d
nigmasilmi <sn.silmi@gmail.com>
2021-10-22 15:37:48 -0300

    Implemented configuration management

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 import { Logger } from '@nestjs/common';
-
 async function bootstrap() {
   const logger = new Logger();
   const port = 3000;
